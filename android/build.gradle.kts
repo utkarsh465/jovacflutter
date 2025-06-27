465ad1ad932e9f2e7455e500a8df9feafbf55e60
utkarsh raj <utkarshr571@gmail.com>
2025-06-27 14:10:12 +0530

    assignment4

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,21 +1,22 @@
-// allprojects {
-//     repositories {
-//         google()
-//         mavenCentral()
-//     }
-// }
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-// val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-// rootProject.layout.buildDirectory.value(newBuildDir)
 
-// subprojects {
-//     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-//     project.layout.buildDirectory.value(newSubprojectBuildDir)
-// }
-// subprojects {
-//     project.evaluationDependsOn(":app")
-// }
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.value(newBuildDir)
 
-// tasks.register<Delete>("clean") {
-//     delete(rootProject.layout.buildDirectory)
-// }
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
